cbbfde10c217a9836ce837dcf9a283e31b55c430
Qiang Sima <qiangsima@outlook.com>
2021-11-18 14:16:05 +0800

    整合MongoDB实现文档操作

---
+++ src/main/java/com/sima/mall/service/MemberReadHistoryService.java
@@ -0,0 +1,22 @@
+package com.sima.mall.service;
+
+import com.sima.mall.nosql.mongodb.document.MemberReadHistory;
+
+import java.util.List;
+
+public interface MemberReadHistoryService {
+    /**
+     * 生成浏览记录
+     */
+    int create(MemberReadHistory memberReadHistory);
+
+    /**
+     * 批量删除浏览记录
+     */
+    int delete(List<String> ids);
+
+    /**
+     * 获取用户浏览历史记录
+     */
+    List<MemberReadHistory> list(Long memberId);
+}
